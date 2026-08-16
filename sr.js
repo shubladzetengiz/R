10d891f327653170ba9a2ff52af75a1e32322140
shubladzetengiz <96029987+shubladzetengiz@users.noreply.github.com>
2026-08-16 14:42:28 +0200

    Update sr.js

--- sr.js
+++ sr.js
@@ -1,21 +1,28 @@
 (function () {
-    // Ждем готовности Lampa
-    function init() {
-        if (!window.Lampa || !Lampa.Menu) return setTimeout(init, 1000);
-        
+    // Используем интервал вместо событий, чтобы точно дождаться загрузки
+    var checkLampa = setInterval(function () {
+        if (window.Lampa && Lampa.Menu && Lampa.Component) {
+            clearInterval(checkLampa);
+            initSelfRadio();
+        }
+    }, 500);
+
+    function initSelfRadio() {
+        // Добавляем пункт в меню
         Lampa.Menu.add({
             title: 'SelfRadio',
             component: 'selfradio',
             page: 'selfradio'
         });
 
+        // Создаем пустую страницу (чтобы не упало)
         Lampa.Component.add('selfradio', function () {
             this.create = function () {
-                return Lampa.Template.get('empty', { title: 'Плагин работает!' });
+                var div = document.createElement('div');
+                div.innerHTML = '<h1>SelfRadio работает!</h1>';
+                div.style.padding = '50px';
+                return div;
             };
         });
-        
-        console.log('SelfRadio plugin initialized');
     }
-    init();
 })();
